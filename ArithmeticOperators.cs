40c8b5e072dceea6197ee3a99565d5ba0a2c157a
Vaibhav <vaibhavsinggh144@gmail.com>
2025-01-20 22:42:14 +0530

    added first program

---
+++ ArithmeticOperators.cs
@@ -0,0 +1,14 @@
+using System;
+
+class ArithmeticOperators {
+static void Main(string[] args) {
+int a = 10;
+int b = 5;
+
+Console.WriteLine("Addition: " + (a + b)); // 15
+Console.WriteLine("Subtraction: " + (a - b)); // 5
+Console.WriteLine("Multiplication: " + (a * b)); // 50
+Console.WriteLine("Division: " + (a / b)); // 2
+Console.WriteLine("Modulus: " + (a % b)); // 0
+}
+}
